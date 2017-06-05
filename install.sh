8ef0118670a44ce1c461040fae9470d359867862
Ross Gardler <ross@gardler.org>
2017-06-05 13:52:29 -0700

    simdem.py needs to be executable

--- install.sh
+++ install.sh
@@ -5,6 +5,7 @@ SYMLINK=simdem
 pip3 install pexpect
 
 sudo cp $FILE $INSTALL_DIR
+sudo chmod +x $INSTALL_DIR$FILE
 
 if [ ! -L $INSTALL_DIR$SYMLINK ]; then
     sudo ln -s $INSTALL_DIR$FILE $INSTALL_DIR$SYMLINK
