56d213fd13d2513bfc1afcd2eec05fc07e8a5865
Ross Gardler <ross@gardler.org>
2017-06-25 07:25:38 +0000

    use data volumes for the azure data - makes it easier to persist between executions

--- scripts/run_novnc.sh
+++ scripts/run_novnc.sh
@@ -1,4 +1,12 @@
+#!/bin/bash
+
 # Runs a headless VNC Docker container with Simdem installed.
+#
+# Usage: run_novnc.sh [SCRPT_DIR]
+#
+# SCRIPT_DIR is an optional parameter to define a directory containing
+# SimDem scripts that will be mounted into the final container. Default
+# value is `demo_scripts`.
 #
 # Connect with a browser at http://YOUR_DOCKER_HOST:8080/?password=vncpassword
 #
@@ -14,12 +22,13 @@ VNC_COL_DEPTH='24'
 VNC_RESOLUTION='1024x768'
 VNC_PW='vncpassword'
 
+SCRIPT_DIR=demo_scripts
 
 docker stop simdem_novnc
 docker rm simdem_novnc
 
 docker run -d -p 5901:5901 -p 8080:6901 --name simdem_novnc \
-       -v ~/.azure:/headless/.azure \
+       --volume azure_data:/headless/.azure \
        -e VNC_COL_DEPTH=$VNC_COL_DEPTH \
        -e VNC_RESOLUTION=$VNC_RESOLUTION \
        -e VNC_PW=$VNC_PW \
