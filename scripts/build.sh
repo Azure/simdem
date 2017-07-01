3750305e27490eff09b59c05ffc4c9962659cc13
Ross Gardler <ross@gardler.org>
2017-07-01 05:18:38 +0000

    if no flavor is provided build both cli and novnc

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,18 +4,30 @@
 #
 # Usage:
 #
-# build.sh            - builds the default container (novnc)
-# TODO: build.sh novnc      - builds the default container (novnc)
+# build.sh                  - builds both containers (cli and novnc)
+# TODO: build.sh novnc      - builds the novnc version of the container
 # TODO: build.sh cli        - builds the CLI version of the container
 
 REPOSITORY=rgardler
-FLAVOR=${1:-novnc}
+FLAVOR=${1:-}
 CONTAINERNAME=simdem_$FLAVOR
 
 VERSION=`grep -Po '(?<=SIMDEM_VERSION = \")(.*)(?=\")' simdem.py`
 
 echo Building $REPOSITORY/$CONTAINERNAME:$VERSION .
 
-docker build -f Dockerfile_$FLAVOR -t $REPOSITORY/$CONTAINERNAME:$VERSION .
+if [[ $FLAVOR == "novnc" ]]; then
+    docker build -f Dockerfile_$FLAVOR -t $REPOSITORY/$CONTAINERNAME:$VERSION .
+    echo Built $REPOSITORY/$CONTAINERNAME:$VERSION .
+elif [[ $FLAVOR == "cli" ]]; then
+    docker build -f Dockerfile_$FLAVOR -t $REPOSITORY/$CONTAINERNAME:$VERSION .
+    echo Built $REPOSITORY/$CONTAINERNAME:$VERSION .
+				  
+else
+    docker build -f Dockerfile_cli -t $REPOSITORY/${CONTAINERNAME}cli:$VERSION .
+    echo Built $REPOSITORY/${CONTAINERNAME}cli:$VERSION .
+    docker build -f Dockerfile_novnc -t $REPOSITORY/${CONTAINERNAME}novnc:$VERSION .
+    echo Built $REPOSITORY/${CONTAINERNAME}novnc:$VERSION .
+fi    
 
-echo Built $REPOSITORY/$CONTAINERNAME:$VERSION .
+
