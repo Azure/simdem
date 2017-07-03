0efa000b509ff87f6a4390f2eea32f6454851f66
Ross Gardler <ross@gardler.org>
2017-07-03 05:48:59 +0000

    detext a filed build and report it

--- scripts/build.sh
+++ scripts/build.sh
@@ -16,18 +16,27 @@ VERSION=`grep -Po '(?<=SIMDEM_VERSION = \")(.*)(?=\")' simdem.py`
 
 echo Building $REPOSITORY/$CONTAINERNAME:$VERSION .
 
+
+print_result() {
+    if [ $1 -eq 0 ]; then
+	echo "Built $2"
+    else
+	echo "Failed to build $2"
+	exit 1
+    fi
+}
+
+
 if [[ $FLAVOR == "novnc" ]]; then
     docker build -f Dockerfile_$FLAVOR -t $REPOSITORY/$CONTAINERNAME:$VERSION .
-    echo Built $REPOSITORY/$CONTAINERNAME:$VERSION .
+    print_result $?  "$REPOSITORY/$CONTAINERNAME:$VERSION ."
 elif [[ $FLAVOR == "cli" ]]; then
     docker build -f Dockerfile_$FLAVOR -t $REPOSITORY/$CONTAINERNAME:$VERSION .
-    echo Built $REPOSITORY/$CONTAINERNAME:$VERSION .
+    print_result  $? "$REPOSITORY/$CONTAINERNAME:$VERSION ."
 				  
 else
     docker build -f Dockerfile_cli -t $REPOSITORY/${CONTAINERNAME}cli:$VERSION .
-    echo Built $REPOSITORY/${CONTAINERNAME}cli:$VERSION .
+    print_result $?  "$REPOSITORY/${CONTAINERNAME}cli:$VERSION ."
     docker build -f Dockerfile_novnc -t $REPOSITORY/${CONTAINERNAME}novnc:$VERSION .
-    echo Built $REPOSITORY/${CONTAINERNAME}novnc:$VERSION .
+    print_result $?  "$REPOSITORY/${CONTAINERNAME}novnc:$VERSION ."
 fi    
-
-
