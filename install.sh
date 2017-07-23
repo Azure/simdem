7cd827d6cff57bcfdeea0249bd71a87eadcda9ac
Ross Gardler <ross@gardler.org>
2017-07-23 19:18:44 +0000

    remove simdem.py, no longer used in 0.7+

--- install.sh
+++ install.sh
@@ -1,13 +1,26 @@
+# Remove < 0.7.0 version
+if [ -f ~/bin/simdem.py ]; then
+    echo "Since version 0.7.0 Simdem is installed in a different way, we need to remove the old Simdem version."
+    rm ~/bin/simdem.py
+    rm ~/bin/simdem
+fi
+
+if [ -f /usr/local/bin/simdem.py ]; then
+    echo "Since version 0.7.0 Simdem is installed in a different way, we need to remove the old Simdem version."
+    sudo rm /usr/local/bin/simdem.py
+    sudo rm /usr/local/bin/simdem
+fi
+
 if [ -f /.dockerenv ]; then
     echo "Running in a Docker container"
     IS_DOCKER=true
-    INSTALL_DIR=~/bin/
+    INSTALL_DIR=~/bin/simdem-dev/
 else
     echo "Not running in a Docker container"
     IS_DOCKER=false
-    INSTALL_DIR=/usr/local/bin/
+    INSTALL_DIR=/usr/local/bin/simdem-dev/
 fi
-FILE=simdem.py
+MAIN_FILE=main.py
 SYMLINK=simdem
 
 if [[ "$OSTYPE" == "darwin"* ]]; then
@@ -22,20 +35,22 @@ fi
 
 pip3 install -r requirements.txt
 
-mkdir -p $INSTALL_DIR
-
 if [ "$IS_DOCKER" = true ]; then
-    cp $FILE $INSTALL_DIR
-    chmod +x $INSTALL_DIR$FILE
+    mkdir -p $INSTALL_DIR
+    
+    cp -r * $INSTALL_DIR
+    chmod +x $INSTALL_DIR$MAIN_FILE
 
-    if [ ! -L $INSTALL_DIR$SYMLINK ]; then
-	ln -s $INSTALL_DIR$FILE $INSTALL_DIR$SYMLINK
+    if [ ! -L $INSTALL_DIR../$SYMLINK ]; then
+	ln -s $INSTALL_DIR$MAIN_FILE $INSTALL_DIR../$SYMLINK
     fi
 else
-    sudo cp $FILE $INSTALL_DIR
-    sudo chmod +x $INSTALL_DIR$FILE
+    sudo mkdir -p $INSTALL_DIR
 
-    if [ ! -L $INSTALL_DIR$SYMLINK ]; then
-	sudo ln -s $INSTALL_DIR$FILE $INSTALL_DIR$SYMLINK
+    sudo cp -r * $INSTALL_DIR
+    sudo chmod +x $INSTALL_DIR$MAIN_FILE
+
+    if [ ! -L $INSTALL_DIR../$SYMLINK ]; then
+	sudo ln -s $INSTALL_DIR$MAIN_FILE $INSTALL_DIR../$SYMLINK
     fi
 fi
