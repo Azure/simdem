2dd61beab71b7186cfbb3d79f2a793f36a33dcdd
Ross Gardler <ross@gardler.org>
2017-08-07 05:05:39 +0000

    use input instead of textarea (can't get textarea to take focus)

--- js/console.js
+++ js/console.js
@@ -43,25 +43,27 @@ function consoleMonitorSocket() {
     socket.on('input_string', function(msg) {
 	in_str = ""
 	$('#input_string_wrap').remove()
-	textarea = $('<textarea id="input_string" contenteditable="true" class="console_input"/>').text("FIXME: Give this box focus and show cursor");
-	div = $('<div class="twrap" id="input_string_wrap">').append(textarea);
+	in_element = $('<input type="text" id="input_string" contenteditable="true" class="console_input" placeholder = "command (default Tutorial)"/>');
+	div = $('<div class="twrap" id="input_string_wrap">').append(in_element);
 	$('#info').append(div)
-
-	textarea.on('change', function() {
+	
+	in_element.on('change', function() {
 	    in_str = $('#input_string').val()
 	    log("GET_INPUT", "Got '" + in_str + "'")
 	    socket.emit('input_string', in_str)
 	});
 
-	textarea.keypress(function(event) {
+	in_element.keypress(function(event) {
 	    if (event.which == 13) {
-		textarea.change()
+		in_element.change()
 	    }
 	})
 
-	textarea.blur(function(){
+	in_element.blur(function(){
 	    $('#input_string_wrap').remove()
 	});
+
+	in_element.focus();
     });
     
     socket.on('clear', function(msg) {
