931173fc0ddb470c9fd19f7f1cd9dacb04b51a20
Tommy Falgout <tommy@lastcoolnameleft.com>
2017-11-22 23:38:54 -0600

    refactor config

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
-from simdem import core,config
+from simdem import core
 import os
 import sys
 import logging
 import optparse
+import configparser
 
 def main():
-    p = optparse.OptionParser("%prog <options> file", version=config.SIMDEM_VERSION)
+    p = optparse.OptionParser("%prog <options> file", version="%prog 1.0")
     p.add_option('--debug', '-d', action="store_true",
                  help="Turn on logging to console")
+    p.add_option('--config-file', '-f', default="content/config/demo.ini",
+                 help="Config file to use")
     options, arguments = p.parse_args()
 
     validate_error = validate(options, arguments)
@@ -15,7 +18,10 @@ def main():
         print(validate_error)
         exit(1)
 
-    setup_logging(options)
+    config = configparser.ConfigParser()
+    config.read(options.config_file)
+
+    setup_logging(config, options)
 
     simdem = core.Core()
 
@@ -28,14 +34,18 @@ def validate(options, arguments):
 
     file_path = arguments[0]
     if not os.path.isfile(file_path):
-        return "Unable to find file " + file_pathu
+        return "Unable to find file " + file_path
 
-def setup_logging(options):
-    logFormatter = logging.Formatter(config.LOG_FORMAT)
+    if not os.path.isfile(options.config_file):
+        return "Unable to find config file " + options.config_file
+
+
+def setup_logging(config, options):
+    logFormatter = logging.Formatter(config.get('LOG', 'FORMAT', raw=True))
     rootLogger = logging.getLogger()
-    rootLogger.setLevel(config.LOG_LEVEL)
+    rootLogger.setLevel(config.get('LOG', 'LEVEL'))
 
-    fileHandler = logging.FileHandler(config.LOG_FILE)
+    fileHandler = logging.FileHandler(config.get('LOG', 'FILE'))
     fileHandler.setFormatter(logFormatter)
     rootLogger.addHandler(fileHandler)
 
