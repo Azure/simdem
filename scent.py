ed477695b9cdf945362e4a9a929bcfa076847a8a
Tommy Falgout <tommy@lastcoolnameleft.com>
2017-12-21 23:39:02 -0600

    add scent.py

---
+++ scent.py
@@ -0,0 +1,34 @@
+from sniffer.api import * # import the really small API
+import os, termstyle
+
+# you can customize the pass/fail colors like this
+pass_fg_color = termstyle.green
+pass_bg_color = termstyle.bg_default
+fail_fg_color = termstyle.red
+fail_bg_color = termstyle.bg_default
+
+# All lists in this variable will be under surveillance for changes.
+watch_paths = ['.']
+
+# this gets invoked on every file that gets changed in the directory. Return
+# True to invoke any runnable functions, False otherwise.
+#
+# This fires runnables only if files ending with .py extension and not prefixed
+# with a period.
+@file_validator
+def py_files(filename):
+    return (filename.endswith('.py') or filename.endswith('.md')) and not os.path.basename(filename).startswith('.')
+
+# This gets invoked for verification. This is ideal for running tests of some sort.
+# For anything you want to get constantly reloaded, do an import in the function.
+#
+# sys.argv[0] and any arguments passed via -x prefix will be sent to this function as
+# it's arguments. The function should return logically True if the validation passed
+# and logicially False if it fails.
+#
+# This example simply runs nose.
+@runnable
+def execute_nose(*args):
+    import nose
+    return nose.run(argv=list(args))
+
