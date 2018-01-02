fa4ddb001ab0953c3ef8cf2443d053aca5fed6c4
Tommy Falgout <tommy@lastcoolnameleft.com>
2018-01-02 15:24:57 -0600

    refactor import statements
    Signed-off-by: Tommy Falgout <tommy@lastcoolnameleft.com>

--- tests/context.py
+++ tests/context.py
@@ -1,11 +1,10 @@
 # -*- coding: utf-8 -*-
 
-import os
 import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 import simdem
-from simdem.executor import bash
-from simdem.parser import codeblock, context
 from simdem.render import demo
-
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from simdem.parser import codeblock,context
+from simdem.executor import bash
