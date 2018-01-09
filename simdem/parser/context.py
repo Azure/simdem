76e6ee482e57a2bb9582a881c6e6fac0549de1ef
miyuchina <miyu.china@icloud.com>
2018-01-08 21:23:47 -0600

    Refactored ContextParser with custom token class

--- simdem/parser/context.py
+++ simdem/parser/context.py
@@ -1,122 +1,87 @@
 """ This module hosts the ContextParser
 """
 
-import logging
+import logging    # XXX: implement proper logging
 
-import mistletoe.ast_renderer as renderer
-import mistletoe.block_token as token
+from mistletoe.block_token import BlockToken, Document, tokenize
+from mistletoe.base_renderer import BaseRenderer
 
-
-class ContextParser(object):
-    """ This class parses the human readable markdown using a defined syntax to
-        know how to create the SimDem Execution Object
-        and uses the code block language to know which type of execution it is
-    """
-
-    @staticmethod
-    def is_command_block(block):
-        """ Expects a code block with a shell command in it
-        """
-        if block['type'] == 'BlockCode' and block['language'] == 'shell':
-            return True
-        return False
+class Section(BlockToken):
+    def __init__(self, lines):
+        self.heading = lines[0].strip().split('# ')[1].lower()
+        # self.children contains other block tokens in this section
+        super().__init__(lines[1:], tokenize)
 
     @staticmethod
-    def is_result_block(block):
-        """ This is different than previous SimDem because it didn't require
-            a language for the result.
-            I believe this approach is more declarative.
-        """
-        if block['type'] == 'BlockCode' and block['language'] == 'result':
-            return True
-        return False
+    def start(line):
+        return line.startswith('#')
 
     @staticmethod
-    def is_next_step_block(block):
-        """ This block is similiar to "choose your own adventure" games
-            This feature allows you to determine which document you want to process
-            after you complete the current one
-        """
-        if 'children' in block and block['children'] \
-            and 'content' in block['children'][0] \
-            and 'next step' in block['children'][0]['content'].lower() \
-            and block['type'].lower() == 'heading':
-            return True
-        return False
+    def read(lines):
+        """Read until before the next heading."""
+        line_buffer = []
+        while lines.peek() is not None and not lines.peek().startswith('#'):
+            line_buffer.append(next(lines))
+        return line_buffer
 
-    @staticmethod
-    def is_prerequisite_block(block):
-        """
-        I'm not a fan of denoting prerequisites by using a header title,
-        but that will suffice for now
-        Will look like this coming out of AST
-        {'children': [{'children': [{'content': 'Prerequisites', 'type': 'RawText'}],
-                'level': 1,
-                'type': 'Heading'},
-        """
-        if 'children' in block and block['children'] \
-            and 'content' in block['children'][0] \
-            and 'prerequisite' in block['children'][0]['content'].lower() \
-            and block['type'].lower() == 'heading':
-            return True
-        return False
 
-    def parse_file(self, file_path):
-        """ The main meat for parsing the file.  Uses mistletoe's AST parser
-            to create a tokenized object and then parses that tokenized object
-            into SimDem Execution Object
-        """
-        with open(file_path, 'r') as fin:
-            ast = renderer.get_ast(token.Document(fin))
-
-        res = {
+class ContextParser(BaseRenderer):
+    def __init__(self):
+        self.res = {
             'prerequisites': [],
             'commands': []
         }
+        self._in_prereq = False
+        self._in_next_steps = False
+        # add Section to the tokenizing process
+        super().__init__(Section)
 
-        idx = 0
-        blocks = ast['children']
-        #logging.debug("parse_file():processing " + str(blocks)
-        while idx < len(blocks):
-            block = blocks[idx]
-            logging.debug("parse_file():processing " + str(block))
+    def render_section(self, token):
+        if 'prerequisites' in token.heading.lower(): # prereq section
+            self._in_prereq = True
+        elif 'next steps' in token.heading.lower():  # next steps section
+            self._in_next_steps = True
+        rendered = self.render_inner(token)
+        self._in_prereq = False
+        self._in_next_steps = False
+        return rendered
 
-            if self.is_prerequisite_block(block):
-                logging.debug("parse_file():found preqreq block")
-                res['prerequisites'] = \
-                    [x['children'][0]['target'] for x in blocks[idx+1]['children']]
-                #  No need to process the next block since that's the prereqs
-                idx = idx + 1
+    def render_link(self, token):
+        rendered = self.render_inner(token)
+        if self._in_prereq:
+            self.res['prerequisites'].append(token.target)
+        elif self._in_next_steps:
+            step = {'title': rendered, 'target': token.target}
+            try:
+                self.res['next_steps'].append(step)
+            except KeyError:
+                self.res['next_steps'] = [step]
+        return rendered
 
-            elif self.is_result_block(block):
-                # Assume that the result block is for the previous command block
-                logging.debug("parse_file():is_result_block")
-                content = block['children'][0]['content'].rstrip()
-                if content:
-                    res['commands'][len(res['commands']) - 1]['expected_result'] = content
+    def render_block_code(self, token):
+        if token.language == 'shell':     # commands
+            lines = token.children[0].content.splitlines()
+            self.res['commands'].extend({'command': line} for line in lines)
+        elif token.language == 'result':  # expected result
+            line = token.children[0].content.strip()
+            self.res['commands'][-1]['expected_result'] = line
+        return self.render_inner(token)
 
-            elif self.is_command_block(block):
-                logging.debug("parse_file():is_command_block")
-                for line in block['children'][0]['content'].split("\n"):
-                    if line:
-                        res['commands'].append({'command': line})
+    def render_raw_text(self, token):
+        return token.content
 
-            elif self.is_next_step_block(block):
-                logging.debug("parse_file():is_next_step_block")
-                block = blocks[idx]
-                # Fast-forward to find the list block inside this header block.
-                # Maybe we should also test to verify we haven't gone past the header block?
-                while 'List' not in block['type'] and idx < len(blocks):
-                    idx = idx + 1
-                    block = blocks[idx]
-                print(block)
-                res['next_steps'] = [{'target': x['target'], 'title': x['children'][0]['content']}
-                                     for x in block['children'][0]['children']
-                                     if x['type'] == 'Link']
+    def __getattr__(self, name):
+        """
+        All other render functions returns the result of render_inner.
+        """
+        if name.startswith("render"):
+            return self.render_inner
+        # if not accessing a render function, raise AttributeError
+        error_msg = "'{}' object has no attribute '{}'".format(self.__class__.__name__, name)
+        raise AttributeError(error_msg)
 
-            else:
-                logging.info("get_commands():unknown_block.  Ignoring")
+    def parse_file(self, file_path):
+        with open(file_path, 'r') as fin:
+            self.render(Document(fin))
+        return self.res
 
-            idx = idx + 1
-            #logging.debug(res)
-        return res
