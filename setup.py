f0969a052c5f588bdc2e789c8d61f2f0a917a46c
Tommy Falgout <tommy@lastcoolnameleft.com>
2018-01-28 18:01:49 -0600

    Add ability to use default ini file
    Signed-off-by: Tommy Falgout <tommy@lastcoolnameleft.com>

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+""" Setup.py """
 
 # Created from https://github.com/kennethreitz/setup.py
 
@@ -6,20 +7,20 @@ from setuptools import setup, find_packages
 
 
 with open('README.md') as f:
-    readme = f.read()
+    README = f.read()
 
 with open('LICENSE') as f:
-    license = f.read()
+    LICENSE = f.read()
 
 setup(
     name='simdem',
     version='0.1.0',
     description='SimDem',
-    long_description=readme,
+    long_description=README,
     author='Tommy Falgout/Ross Gardler',
     author_email='thfalgou@microsoft.com',
     url='https://github.com/Azure/simdem',
-    license=license,
+    license=LICENSE,
     packages=find_packages(exclude=('tests', 'docs')),
     entry_points={
         'console_scripts': [
@@ -27,4 +28,3 @@ setup(
         ]
     }
 )
-
--- simdem/cli.py
+++ simdem/cli.py
@@ -4,6 +4,7 @@ import configparser
 import logging
 import argparse
 import os
+import pkg_resources
 
 from simdem.executor import bash
 from simdem.parser import ast, simdem1
@@ -17,7 +18,7 @@ def main():
                       help='file to process')
     argp.add_argument('--debug', '-d', action="store_true",
                       help="Turn on logging to console")
-    argp.add_argument('--config-file', '-c', default="content/config/demo.ini",
+    argp.add_argument('--config-file', '-c',
                       help="Config file to use")
     argp.add_argument('--mode', '-m', default="tutorial",
                       help="Mode to use", choices=['demo', 'dump', 'test', 'tutorial'])
@@ -30,19 +31,25 @@ def main():
     options = argp.parse_args()
 
     file_path = options.file
-    validate_error = validate(options, file_path)
-    if validate_error:
-        print(validate_error)
-        exit(1)
+    config_file_path = get_config_file_path(options)
+    validate(file_path, config_file_path)
 
     config = configparser.ConfigParser()
-    config.read(options.config_file)
+    config.read(config_file_path)
     inject_arguments(options, config)
 
     setup_logging(config, options)
 
     mode = get_mode(options, config)
     mode.process_file(file_path)
+
+def get_config_file_path(options):
+    """ Returns the found config file path """
+    options_config_file = options.config_file
+    if options_config_file:
+        return options_config_file
+    file_path = pkg_resources.resource_filename(__name__, 'simdem.ini')
+    return file_path
 
 def inject_arguments(options, config):
     """ Injects CLI arguments into config settings """
@@ -51,13 +58,13 @@ def inject_arguments(options, config):
         [section, option] = key.split('.')
         config.set(section, option, value)
 
-def validate(options, file_path):
+def validate(file_path, config_file_path):
     """ validate all passed in arguments """
     if not os.path.isfile(file_path):
-        return "Unable to find file: " + file_path
+        raise FileNotFoundError('Unable to find file: ' + file_path)
 
-    if not os.path.isfile(options.config_file):
-        return "Unable to find config file: " + options.config_file
+    if not os.path.isfile(config_file_path):
+        raise FileNotFoundError('Unable to find config file: ' + config_file_path)
 
 def get_mode(options, config):
     """ Returns correct renderer object """
